c0f92206b87ad65f76a0e368460b8fa1722892a0
ValentinR19 <valenrodriguez1907@gmail.com>
2023-11-07 20:35:32 -0300

    Se agregaron importacioes habitacionModule

--- src/habitaciones/habitaciones.module.ts
+++ src/habitaciones/habitaciones.module.ts
@@ -4,10 +4,14 @@ import { HabitacionesController } from './controller/habitaciones.controller';
 import { Habitacion } from '@habitacion-module/models/classes/habitacion.entity';
 import { TipoHabitacion } from '@habitacion-module/models/classes/tipo-habitacion.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TipoHabitacionService } from '@habitacion-module/services/tipo-habitacion.service';
+import { TipoHabitacionesController } from '@habitacion-module/controller/tipos-habitaciones.controller';
+import { HabitacionRepository } from '@habitacion-module/repositories/habitacion.repository';
+import { TipoHabitacionRepository } from '@habitacion-module/repositories/tipo-habitacion.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Habitacion, TipoHabitacion])],
-  controllers: [HabitacionesController],
-  providers: [HabitacionService],
+  controllers: [HabitacionesController, TipoHabitacionesController],
+  providers: [HabitacionService, HabitacionRepository, TipoHabitacionService, TipoHabitacionRepository],
 })
 export class HabitacionesModule {}
