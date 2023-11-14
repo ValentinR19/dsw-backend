5f0cb44ec1876729ecc23a0a9025cb0b4333f500
fcolarte <fcolarte@lasegunda.com.ar>
2023-11-14 00:33:18 -0300

    add get client by document

--- src/clientes/repositories/cliente.repository.ts
+++ src/clientes/repositories/cliente.repository.ts
@@ -3,13 +3,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { DeepPartial, Repository } from 'typeorm';
 
 export class ClienteRepository {
+
   constructor(@InjectRepository(Cliente) private readonly repository: Repository<Cliente>) {}
 
   async create(usuario: DeepPartial<Cliente>): Promise<Cliente> {
     return this.repository.save(usuario);
   }
 
-  async findByDocumento(documento: string) {
+  async findByDocumento(documento: string): Promise<Cliente> {
     return this.repository.findOneOrFail({ where: { documento } });
   }
 }
